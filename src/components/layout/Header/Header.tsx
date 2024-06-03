801f30e74fdd4c094f0ea90085c1d5772fdd4442
Emil-F <e.fagerjord@gmail.com>
2024-06-03 13:33:34 +0200

    initial commit

---
+++ src/components/layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+import NavButton from "./NavButton";
+
+export default function Header() {
+  return (
+    <header className="h-16 bg-slate-700 flex items-center">
+      <h1 className="">TV-web</h1>
+      <NavButton />
+    </header>
+  );
+}
